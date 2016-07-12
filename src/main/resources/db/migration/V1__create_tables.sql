2d02537e6ca57fcc88de4d8b02aa43d9a2a615b4
Hao Pan <hao_pan@outlook.com>
2016-07-12 16:54:18 +0800

    update table product

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -2,6 +2,6 @@ CREATE TABLE products (
   id VARCHAR(255) PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL,
-  price FLOAT,
+  price FLOAT NOT NULL ,
   rating INT
 );

0f16b75745b83e1cd87f82a7ae10066b265d8d25
Hao Pan <hao_pan@outlook.com>
2016-07-12 16:32:13 +0800

    change product repository test

--- src/test/java/com/thoughtworks/api/infrastructure/repositories/ProductRepositoryTest.java
+++ src/test/java/com/thoughtworks/api/infrastructure/repositories/ProductRepositoryTest.java
@@ -1,8 +1,14 @@
 package com.thoughtworks.api.infrastructure.repositories;
 
-import com.google.inject.Inject;
+
+import com.thoughtworks.api.domain.core.Product;
+import com.thoughtworks.api.domain.core.ProductRepository;
+import com.thoughtworks.api.infrastructure.records.ProductRecord;
 import com.thoughtworks.api.support.DatabaseTestRunner;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,9 +19,10 @@ public class ProductRepositoryTest {
   @Inject
   ProductRepository productRepository;
 
+  @Test
   public void should_create_and_get_product() {
     String id = productRepository.generateId();
-    Product product = new Product(id, "desk", "black", 530, 4);
+    Product product = new ProductRecord(id, "desk", "black", 530, 4);
 
     Product createdProduct = productRepository.create(product);
 

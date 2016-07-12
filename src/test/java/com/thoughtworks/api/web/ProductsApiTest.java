fc01508007848c3bf7422cbd30e8948b210bc077
Hao Pan <hao_pan@outlook.com>
2016-07-12 14:54:11 +0800

    add product post test

--- src/test/java/com/thoughtworks/api/web/ProductsApiTest.java
+++ src/test/java/com/thoughtworks/api/web/ProductsApiTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -13,9 +15,15 @@ import static org.hamcrest.Matchers.is;
 @RunWith(ApiTestRunner.class)
 public class ProductsApiTest extends ApiSupport{
   @Test
-  public void should_import_user_success() throws Exception {
+  public void should_import_product_success() throws Exception {
+    Map<String, Object> productInfo = new HashMap<String, Object>() {{
+      put("name", "desk");
+      put("description", "black");
+      put("price", 530);
+      put("rating", 5);
+    }};
 
-    final Response get = get("/products");
-    assertThat(get.getStatus(), is(201));
+    final Response post = post("/users", productInfo);
+    assertThat(post.getStatus(), is(201));
   }
 }

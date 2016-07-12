7d6fc09565e53ed941529ed82571cd3390c21927
Hao Pan <hao_pan@outlook.com>
2016-07-12 16:57:12 +0800

    productMapper save done

--- src/main/java/com/thoughtworks/api/infrastructure/mybatis/mappers/ProductMapper.java
+++ src/main/java/com/thoughtworks/api/infrastructure/mybatis/mappers/ProductMapper.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.api.infrastructure.mybatis.mappers;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface ProductMapper {
+  void save(@Param("id") String id,
+            @Param("name") String name,
+            @Param("description") String description,
+            @Param("price") float price,
+            @Param("rating") int rating);
+
+
 }
